f3349e3b3ebd51be68a265575ecb7871b7942fb5
Shunan Zhao <zashuna@gmail.com>
2019-04-25 17:29:34 -0400

    create file to extract smaller set of customers

---
+++ create_cheq_accts_sample.py
@@ -0,0 +1,20 @@
+import csv
+
+cheq_acct_file = '/STORAGE/cpb/data/CustomersInBothSecondaryAndSample.txt'
+secondary_csv_file = open('/STORAGE/cpb/data/cheqSecondarySummary.csv', 'r', newline='')
+secondary_csv_file_sample = open('/STORAGE/cpb/data/cheqSecondarySummary.csv', 'w', newline='')
+
+sample_writer = csv.writer(secondary_csv_file_sample, delimiter=',')
+secondary_reader = csv.reader(secondary_csv_file)
+
+cheq_accts = set()
+with open(cheq_acct_file, 'r') as f:
+    for line in f:
+        cheq_accts.add(line.strip())
+
+for row in secondary_reader:
+    if row[0] in cheq_accts:
+        sample_writer.writerow(row)
+
+secondary_csv_file_sample.close()
+secondary_csv_file.close()
